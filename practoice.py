0b1539513d1773c2764c1cead1be3a23f80dfbf9
Callum Forsyth <c-forsyth02@sky.com>
2024-10-30 23:07:32 +0000

    Added a match to choose a function
    works like a switch

--- practoice.py
+++ practoice.py
@@ -1,13 +1,40 @@
 import calendar
+import random
 
-print('hello world')
+def Hello():
+    print('hello world')
 
-def greet(name):
-    print('greetings ' + name)
+    def greet(name):
+        print('greetings ' + name)
 
-name = input("Enter name ")
-greet(name)
+    name = input("Enter name ")
+    greet(name)
 
 def printCallender():
     cal = calendar.month(2024, 2)
     print(cal)
+
+def randomInt(x,y):
+    number = random.randint(x,y)
+    print(number)
+
+def ranMovie():
+    movies = ["alladin","avrage avengers", 'endme']
+    print(random.choice(movies))
+    
+function = input("What function ")
+
+match function:
+    case "Hello":
+        Hello()
+    case "Callender":
+        printCallender()
+    case "random range":
+        min = input("Please enter min value ")
+        max = input("Please endter max value ")
+        randomInt(min,max)
+    case "Movie":
+        ranMovie()
+
+
+
